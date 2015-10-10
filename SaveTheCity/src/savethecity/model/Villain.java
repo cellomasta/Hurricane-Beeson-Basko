db3a838545a9802cc4f22a7b72487f5cb18ae985
Alicia <Alicia@Alicia>
2015-10-10 01:12:59 -0600

    captive, hero & villain classes - edits to main() - alicia

---
+++ SaveTheCity/src/savethecity/model/Villain.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package savethecity.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author Alicia
+ */
+public class Villain implements Serializable{
+     private Character character;
+    private Tiles tile;
+    
+    public Villain(){
+    }
+
+    public Character getCharacter() {
+        return character;
+    }
+
+    public void setCharacter(Character character) {
+        this.character = character;
+    }
+
+    public Tiles getTile() {
+        return tile;
+    }
+
+    public void setTile(Tiles tile) {
+        this.tile = tile;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 97 * hash + Objects.hashCode(this.character);
+        hash = 97 * hash + Objects.hashCode(this.tile);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Villain other = (Villain) obj;
+        if (!Objects.equals(this.character, other.character)) {
+            return false;
+        }
+        if (!Objects.equals(this.tile, other.tile)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Villain{" + "character=" + character + ", tile=" + tile + '}';
+    }
+}
