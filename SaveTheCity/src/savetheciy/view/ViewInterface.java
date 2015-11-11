e5fd71f8f642512162f14529b08390474c27d000
Alicia <Alicia@Alicia>
2015-11-10 19:08:15 -0700

    week 9 team

---
+++ SaveTheCity/src/savetheciy/view/ViewInterface.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package savetheciy.view;
+
+/**
+ *
+ * @author Alicia
+ */
+public class ViewInterface {
+    
+    public void display();
+    public String getInput();
+    public boolean doAction(Object obj);
+}
