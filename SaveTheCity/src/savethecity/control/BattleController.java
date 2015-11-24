304c2cf69f04ea27929488ed6805c6133d0df2aa
willnelson <willnelson@192.168.1.159>
2015-11-23 17:24:16 -0800

    VillainHealth ArrayList added.

--- SaveTheCity/src/savethecity/control/BattleController.java
+++ SaveTheCity/src/savethecity/control/BattleController.java
@@ -24,4 +24,21 @@ public class BattleController {
 	return FALSE;
         
     }
+    
+    public ArrayList<Item> addVillainHealth() {
+        
+        //create ArrayList with villainHealth
+        ArrayList<Item> villainHealthList = new ArrayList<>();
+        
+        //add objects to ArrayList
+        Item mystique = new Item("Mystique", 20);
+        villainHealthList.add(mystique);
+        villainHealthList.add(new Item("DoctorOctopus", 20));
+        villainHealthList.add(new Item("MisterFreeze", 25));
+        villainHealthList.add(new Item("GreenGoblin", 25));
+        villainHealthList.add(new Item("Loki", 30));
+        villainHealthList.add(new Item("DemonHead", 50));
+        
+        return villainHealthList;
+    }
 }
