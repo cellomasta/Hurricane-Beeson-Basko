77a496d08b050f299694ddf06a2f0ee36f74e96b
forev <forev@DESKTOP-PQ0OJKG>
2015-12-04 11:11:39 -0700

    Week 10 individual assignment with implementation

--- SaveTheCity/src/savethecity/control/BattleController.java
+++ SaveTheCity/src/savethecity/control/BattleController.java
@@ -6,11 +6,16 @@
 package savethecity.control;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.ObjectInputStream;
 import java.io.PrintWriter;
 import static java.lang.Boolean.FALSE;
 import static java.lang.Boolean.TRUE;
 import java.util.ArrayList;
 import savethecity.SaveTheCity;
+import savethecity.exceptions.BattleControllerException;
+import savethecity.model.Game;
 import savethecity.model.Villain;
 
 /**
@@ -61,5 +66,18 @@ public class BattleController {
         }
         this.console.println("Average Villain attack is " + average + ".");
     }
-    
+ public static void getvillainHealth(String filepath)
+        throws BattleControllerException{
+            Game villainHealth = null;
+
+            try (FileInputStream flips = new FileInputStream(filepath)) {
+                ObjectInputStream output = new ObjectInputStream(flips);
+
+                villainHealth = (Game) output.readObject(); //read the game object from file
+            } catch (FileNotFoundException fnfe) {
+                throw new BattleControllerException(fnfe.getMessage());
+            } catch (Exception e) {
+                throw new BattleControllerException(e.getMessage());
+            }
+        }   
 }
